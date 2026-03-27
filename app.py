b8442bb2a05755dd46d5c8436e12f1526b3092dc
Laura Camila <lauracamilaav1@gmail.com>
2026-03-27 22:04:58 +0800

    Added addTask function and route to /generate

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("Home_page.html", name="Laura")
+
+@app.route('/pomodoriser')
+def pomodoriser():
+    return render_template('Pom.html')
+
+@app.route('/generate', methods=['POST'])
+def addTask():
+    if request.method == 'POST':
+        time_available = request.form.get('time_available')
+        task_name = request.form.get('task_name')
+        taskTimeUnit = request.form.get('taskTimeUnit')
+        task_time = request.form.get('task_time')
+        print(time_available, task_name, taskTimeUnit, task_time)
+        return "Received!"    
 
 if __name__ == '__main__':
     app.run(debug=True)
